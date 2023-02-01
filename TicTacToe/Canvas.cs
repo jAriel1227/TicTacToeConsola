038ed93a5151a17495091868a16cca93644f9f2b
jAriel1227 <20209409@itla.edu.do>
2023-02-01 18:27:16 -0400

    New

--- TicTacToe/Canvas.cs
+++ TicTacToe/Canvas.cs
@@ -12,13 +12,13 @@ namespace TicTacToe
         {
             Console.WriteLine("----Bienvenido al juego-----");
             Console.WriteLine("");
-            Console.WriteLine("   ...................");
+            Console.WriteLine("   ....................");
             Console.WriteLine("C" + "    {0}   |   {1}   |   {2}", b.board[0, 0], b.board[0, 1], b.board[0,2]);
             Console.WriteLine("   --------------------");
             Console.WriteLine("B" + "    {0}   |   {1}   |   {2}", b.board[1, 0], b.board[1, 1], b.board[1, 2]);
             Console.WriteLine("   --------------------");
             Console.WriteLine("A" + "    {0}   |   {1}   |   {2}", b.board[2, 0], b.board[2, 1], b.board[2, 2]);
-            Console.WriteLine("   ...................");
+            Console.WriteLine("   ....................");
             Console.WriteLine("     A     B      C");
             Console.WriteLine("------------------------");
         }
